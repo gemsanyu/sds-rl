9208f98f150cac8f3e04027a7b6c64e703c6ebbb
gemsanyu <git.gemilang.santiyuda@gmail.com>
2022-05-31 15:29:50 +0700

    otw add rl feature extraction

---
+++ rl_policy.py
@@ -0,0 +1,69 @@
+from batsim_py import SimulatorHandler
+from batsim_py.events import HostEvent, SimulatorEvent
+from batsim_py.resources import Host
+
+from batsim_py.monitors import SimulationMonitor, HostStateSwitchMonitor, ConsumedEnergyMonitor, JobMonitor
+
+"""
+kita tiru shutdown policy
+si simulator akan subcribe ke sebuah callback function setiap dtime
+callback function ini isinya:
+1. extract features dari monitor
+2. RL decides to switch on/off
+3. RL executes on or off
+4. 
+"""
+
+class RLPolicy:
+    def __init__(self, 
+                agent, 
+                dtime, 
+                simulation_monitor:SimulationMonitor, 
+                energy_monitor:ConsumedEnergyMonitor,
+                host_monitor:HostStateSwitchMonitor,
+                job_monitor: JobMonitor,
+                simulator: SimulatorHandler) -> None:
+        self.simulator = simulator
+        self.dtime = dtime
+        self.hosts_idle = {}
+        self.agent = agent
+        # Subscribe to some events.
+        self.simulator.subscribe(SimulatorEvent.SIMULATION_BEGINS, self.on_simulation_begins)
+
+        # monitors for extracting features
+        self.simulation_monitor = simulation_monitor
+        self.energy_monitor = energy_monitor
+        self.host_monitor = host_monitor
+        self.job_monitor = job_monitor
+
+    def on_simulation_begins(self, s: SimulatorHandler) -> None:
+        for host in s.platform.hosts:
+            if host.is_idle:
+                self.hosts_idle[host.id] = s.current_time
+                self.setup_callback()
+
+    # def on_host_state_changed(self, h: Host) -> None:
+    #     if h.is_idle and not h.id in self.hosts_idle:
+    #         self.hosts_idle[h.id] = self.simulator.current_time
+    #         self.setup_callback()
+    #     elif not h.is_idle and h.id in self.hosts_idle:
+    #         del self.hosts_idle[h.id]
+
+    def setup_callback(self) -> None:
+        t_next_call = self.simulator.current_time + self.dtime
+        self.simulator.set_callback(t_next_call, self.callback)
+
+    def callback(self, current_time: float) -> None:
+        # print2 feature2 dulu sementara
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", current_time)
+        print(self.job_monitor.info)
+        print()
+        print()
+        print()
+        # compute mean arrival rate
+        # get last 50 elements if exists
+        
+        self.setup_callback()
+        # for host_id, t_idle_start in list(self.hosts_idle.items()):
+        #     if  current_time - t_idle_start >= self.t_timeout:
+        #         self.simulator.switch_off([host_id])
--- train.py
+++ train.py
@@ -1,37 +1,93 @@
-from agent.agent import Agent
+import csv
+import sys
+import pathlib
+from time import time
 
 from batsim_py import SimulatorHandler
-from batsim_py.monitors import SimulationMonitor, HostStateSwitchMonitor, ConsumedEnergyMonitor
+from batsim_py import SimulatorHandler
+from batsim_py.monitors import SimulationMonitor, HostStateSwitchMonitor, ConsumedEnergyMonitor, JobMonitor
 
-def run_simulation(workload_filename):
-    agent = Agent()
+from easy_backfilling import EASYScheduler
+from rl_policy import RLPolicy
+from config import define_args_parser
+
+
+def run_simulation(shutdown_policy, workload_filename, platform_path):
     simulator = SimulatorHandler()
+    scheduler = EASYScheduler(simulator)
 
     # 1) Instantiate monitors to collect simulation statistics
     sim_mon = SimulationMonitor(simulator)
     host_mon = HostStateSwitchMonitor(simulator)
     e_mon = ConsumedEnergyMonitor(simulator)
+    job_mon = JobMonitor(simulator)
+
+    policy = RLPolicy(None, 500, sim_mon, e_mon, host_mon, job_mon, simulator)
+    
 
     # 2) Start simulation
-    simulator.start(platform="platform.xml",
-                    workload=workload_filename,
-                    verbosity="information")
+    simulator.start(platform=platform_path, workload=workload_filename, verbosity="information")
 
     # 3) Schedule all jobs
     while simulator.is_running:
-        for job in simulator.queue:
-            available = simulator.platform.get_not_allocated_hosts()
-            if job.res <= len(available):
-                allocation = [h.id for h in available[:job.res]]
-                simulator.allocate(job.id, allocation)
-        
-
-        simulator.proceed_time()
-
+        scheduler.schedule()
+        simulator.proceed_time()  # proceed directly to the next event.
     simulator.close()
-
+    
     # 4) Return/Dump statistics
     return sim_mon, host_mon, e_mon, simulator
 
-if __name__ = "__main__":
-    sim_mon, host_mon, e_mon, simulator = run_simulation("workloads-gaia.json")
+def F(mean_slowdown , consumed_joules, max_consumed_joules, alpha, beta, is_normalized):
+    if is_normalized:
+        consumed_joules = consumed_joules/max_consumed_joules
+    return alpha * mean_slowdown + beta * consumed_joules
+
+def compute_score(sim_mon, sim_handler, alpha=0.5, beta=0.5, is_normalized=True):
+  platform = sim_handler.platform
+  hosts = platform.hosts
+  total_max_watt_per_min = 0
+  for host in hosts:
+    max_watt_per_min = 0
+    for pstate in host.pstates:
+      max_watt_per_min = max(max_watt_per_min, pstate.watt_full)
+    total_max_watt_per_min += max_watt_per_min
+
+  total_time = sim_handler.current_time
+  max_consumed_joules = total_time*total_max_watt_per_min
+  consumed_joules = sim_mon.info["consumed_joules"]
+  mean_slowdown = sim_mon.info["mean_slowdown"]
+  score = F(mean_slowdown, consumed_joules, max_consumed_joules, alpha, beta, is_normalized)
+  return score
+
+if __name__ == "__main__":
+    
+    parser = define_args_parser()
+    args = parser.parse_args(sys.argv[1:])
+    to_policy = lambda s: TimeoutPolicy(args.timeout, s)
+    start = time()
+    sim_mon, host_mon, e_mon, simulator = run_simulation(to_policy, args.workload_path, args.platform_path) # Simulation 1: Timeout (30 minute) Dataset (Gaia)
+    end = time()
+    walltime = end-start
+    header = ['dataset', 'timeout', 'f(1,0)=slowdown', 'f(0,1)=energy', 'f(0.5,0.5)=balance', "experiment walltime"]
+
+    data = []
+    row = []
+    row.append(args.workload_path)
+    row.append(args.timeout)
+    row.append(compute_score(sim_mon, simulator, 1, 0))
+    row.append(compute_score(sim_mon, simulator, 0, 1))
+    row.append(compute_score(sim_mon, simulator, 0.5, 0.5))
+    row.append(walltime)
+    data.append(row)
+
+    result_dir = pathlib.Path('.')/"results"
+    result_dir.mkdir(exist_ok=True)
+    result_path = result_dir/(args.title+".csv")
+    with open(result_path, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        writer.writerow(header)
+
+        # write multiple rows
+        writer.writerows(data)
